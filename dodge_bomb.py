47f28686d91fbf602c93b6683669e1c3c5474cee
shishido <c0a2317428@edu.teu.ac.jp>
2024-04-23 17:58:48 +0900

    下に２行空行があったほうがいいと思います　#1

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -3,13 +3,16 @@ import random
 import sys
 import pygame as pg
 import time
-WIDTH, HEIGHT = 1600, 900
+WIDTH, HEIGHT = 1600, 900 
+
 DELTA = {  # 移動量辞書（押下キー：移動量タプル）
     pg.K_UP: (0, -5),
     pg.K_DOWN: (0, +5),
     pg.K_LEFT: (-5, 0),
     pg.K_RIGHT: (+5, 0),
 }
+
+
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 def check_bound(obj_rct:pg.Rect) -> tuple[bool, bool]:
     """
@@ -20,6 +23,8 @@ def check_bound(obj_rct:pg.Rect) -> tuple[bool, bool]:
     yoko, tate = True, True
     if obj_rct.left < 0 or WIDTH < obj_rct.right: 
         yoko = False
+
+        
     if obj_rct.top < 0 or HEIGHT < obj_rct.bottom:
         tate = False
     return yoko, tate
